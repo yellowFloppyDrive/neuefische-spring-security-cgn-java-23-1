adfd0f4ab0d3e9674961cd5f75c6a4f92b2b81f6
bitbytejoy <bitbytejoy@gmail.com>
2023-03-06 16:24:28 +0100

    feat: Signup

--- src/main/java/de/neuefische/neuefischespringsecuritycgnjava231/MongoUserController.java
+++ src/main/java/de/neuefische/neuefischespringsecuritycgnjava231/MongoUserController.java
@@ -1,20 +1,54 @@
 package de.neuefische.neuefischespringsecuritycgnjava231;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class MongoUserController {
     private final MongoUserRepository mongoUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     public MongoUser create (@RequestBody MongoUser user) {
-        return mongoUserRepository.save(user);
+        if (user.username() == null || user.username().length() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username is required");
+        }
+
+        if (user.password() == null || user.password().length() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password is required");
+        }
+
+        if (mongoUserRepository.existsByUsername(user.username())) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "User already exists"
+            );
+        }
+
+        MongoUser newUser = new MongoUser(
+                UUID.randomUUID().toString(),
+                user.username(),
+                passwordEncoder.encode(user.password()),
+                "BASIC"
+        );
+
+        MongoUser out = mongoUserRepository.save(newUser);
+
+        return new MongoUser(
+                out.id(),
+                out.username(),
+                null,
+                out.role()
+        );
     }
 
     @GetMapping("/me")
