2972393736776c88847161a297ef415ee651fa3d
bitbytejoy <bitbytejoy@gmail.com>
2023-03-07 15:44:29 +0100

    feat: Session management ALWAYS

--- src/main/java/de/neuefische/neuefischespringsecuritycgnjava231/SecurityConfig.java
+++ src/main/java/de/neuefische/neuefischespringsecuritycgnjava231/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,9 +30,12 @@ public class SecurityConfig {
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .csrfTokenRequestHandler(requestHandler))
                 .httpBasic().and()
+                .sessionManagement(config ->
+                    config.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/api/csrf").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/status/admin").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/users/login").authenticated()
                 .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/status").permitAll()
                 .requestMatchers("/api/**").authenticated()
