f9b082e0bc04a81dc8a52dca27fdca7758d95837
bitbytejoy <bitbytejoy@gmail.com>
2023-03-07 10:07:56 +0100

    feat: CSRF Token

--- src/main/java/de/neuefische/neuefischespringsecuritycgnjava231/SecurityConfig.java
+++ src/main/java/de/neuefische/neuefischespringsecuritycgnjava231/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -19,8 +21,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+
         return http
-                .csrf().disable()
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(requestHandler))
                 .httpBasic().and()
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/api/status/admin").hasRole("ADMIN")
--- src/test/java/de/neuefische/neuefischespringsecuritycgnjava231/MongoUserControllerTest.java
+++ src/test/java/de/neuefische/neuefischespringsecuritycgnjava231/MongoUserControllerTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,5 +27,21 @@ class MongoUserControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("user"));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser
+    void create_whenValid_then200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": "test",
+                                    "password": "test"
+                                }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isOk());
     }
 }
